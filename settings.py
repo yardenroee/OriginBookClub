5744e4bd4a3e0afcce15f881baca8721e26cc596
yardenroee <yardenroee@gmail.com>
2020-01-26 23:44:09 -0500

    register form works

--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'bookclub.apps.BookClubConfig',
     'django.contrib.admin',
     'django.contrib.auth',
