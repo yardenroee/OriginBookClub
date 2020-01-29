6f3e1049c3f4a7b492d7174ffba3acbeac5b6637
yardenroee <yardenroee@gmail.com>
2020-01-29 00:28:50 -0500

    fixed pillow error

--- favorites/signals.py
+++ favorites/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from favorites.models import Favorites
+from .models import Favorites
 
 @receiver(post_save, sender=User) # on creation of user will trigger favorite list creation
 def create_favorites(sender, instance, created, **kwargs):
--- favorites/views.py
+++ favorites/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Favorites
+from django.views.generic import ListView
 # Create your views here.
