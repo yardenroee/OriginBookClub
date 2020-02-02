8ee1b10d15947a32b692a1e1df17efd601e7680d
yardenroee <yardenroee@gmail.com>
2020-02-02 15:35:08 -0500

    working search bar

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from .views import BookDetailView, BookUpdateView, BookListView, BookCreateView, BookDeleteView
+from django.urls import path, re_path
+from .views import BookDetailView, BookUpdateView, BookListView, BookCreateView, BookDeleteView, SearchListView
 from users.views import FavoriteListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name="index"),
+    re_path('search', SearchListView.as_view(), name="search"),
     path('book/new/', BookCreateView.as_view(), name="add_book"),
     path('book/<int:pk>/update', BookUpdateView.as_view(), name="book_update"),
     path('book/<int:pk>/delete', BookDeleteView.as_view(), name="book_delete"),
