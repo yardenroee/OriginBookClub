562a86f0fe66575fb583e210251160f43253f429
yardenroee <yardenroee@gmail.com>
2020-02-02 15:48:49 -0500

    change routing

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 from .views import BookDetailView, BookUpdateView, BookListView, BookCreateView, BookDeleteView, SearchListView
 from users.views import FavoriteListView
-
+from django.conf.urls import url
 urlpatterns = [
     path('', BookListView.as_view(), name="index"),
-    re_path('search', SearchListView.as_view(), name="search"),
+    url('search', SearchListView.as_view(), name="search"),
     path('book/new/', BookCreateView.as_view(), name="add_book"),
     path('book/<int:pk>/update', BookUpdateView.as_view(), name="book_update"),
     path('book/<int:pk>/delete', BookDeleteView.as_view(), name="book_delete"),
